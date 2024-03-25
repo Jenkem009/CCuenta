ddadfa3d06beb7ccc549b854f00e152a46f81303
Pepe Gotera <Pepe Gotera@DESKTOP-VQL1PTA>
2024-03-25 20:37:27 +0100

    PROYECTO FINALIZADO

--- entidadFinanciera/Main.java
+++ entidadFinanciera/Main.java
@@ -2,8 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package entidadFinanciera;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Main {
@@ -14,7 +16,34 @@ public class Main {
         objetoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = objetoCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
+        double cantidad = 0;
+        double cantidadEsperada = 0;
         
+        probarretirar(objetoCuenta, cantidad);
+        probaringresar(objetoCuenta, cantidad): 50);
+    }
+
+    public static void probarretirar(CCuenta objetoCuenta, double cantidad) {
+        probarretirar(objetoCuenta, cantidad, null);
+    }
     
+    public static void probarretirar(CCuenta objetoCuenta, double cantidad, float cantidadEsperada) {
+        try {
+            objetoCuenta.retirar(cantidad);
+        }catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void probaringresar(CCuenta objetoCuenta, double cantidad) {
+        probaringresar(objetoCuenta, cantidad, null);
+    }
+
+    public static void probaringresar(CCuenta objetoCuenta, double cantidad, java.lang.Double cantidadEsperada) {
+        try {
+            objetoCuenta.ingresar(cantidad);
+        }catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
